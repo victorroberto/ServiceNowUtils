c33b0ec7fb4cc7930f1a5ceeca826323129170bb
Victor Roberto da Silva Santos <victorroberto@outlook.com>
2022-12-15 16:06:31 -0300

    Update ServiceNow Code Snippets
    Update ServiceNow Code Snippets

--- serviceNowCodeSnippets.js
+++ serviceNowCodeSnippets.js
@@ -1,6 +1,6 @@
-/* ServiceNow Code Snippets */
+/* ******************** ServiceNow Code Snippets ******************** */
 
-/* Client Scripts */
+/* ******************** Client Scripts ******************** */
 
 function onLoad() {
   //Type appropriate comment here, and begin script below
@@ -21,81 +21,223 @@ function onCellEdit(sysIDs, table, oldValues, newValue, callback) {
   //Type appropriate comment here, and begin script below
 }
 
-/* g_form */
 
-g_form.getValue("fieldName");
-g_form.setValue("fieldName", "value");
+/* ******************** g_form ******************** */
 
-g_form.setMandatory("fieldName", true);
-g_form.setReadOnly("fieldName", true);
-g_form.setVisible("fieldName", true);
+g_form.getValue('fieldName');
+g_form.setValue('fieldName', 'value');
 
-g_form.addOption("fieldName", "value");
-g_form.removeOption("fieldName", "value");
+g_form.setMandatory('fieldName', true);
+g_form.setReadOnly('fieldName', true);
+g_form.setVisible('fieldName', true);
 
-g_form.addErrorMessage("Message");
-g_form.addFormMessage("info message", "info/warning/error");
+g_form.addOption('fieldName', 'value');
+g_form.removeOption('fieldName', 'value');
 
-g_form.showFieldMsg("Message", "error/warn/info");
-g_form.hideFieldMsg("fieldName", true);
+g_form.addErrorMessage('Message');
+g_form.addFormMessage('info message', 'info/warning/error');
+
+g_form.showFieldMsg('Message', 'error/warn/info');
+g_form.hideFieldMsg('fieldName', true);
 
 g_form.clearMessages();
-g_form.clearOptions("fieldName");
+g_form.clearOptions('fieldName');
 g_form.clearAllFormMessages();
 
-g_form.clearOptions("fieldName");
-g_form.clearValue("fieldName");
+g_form.clearOptions('fieldName');
+g_form.clearValue('fieldName');
 
-g_form.getValue("fieldName");
-g_form.getDisplayValue("fieldName");
+g_form.getValue('fieldName');
+g_form.getDisplayValue('fieldName');
 g_form.getUniqueValue();
 
 g_form.isNewRecord();
 
-/* g_user */
+
+/* ******************** g_user ******************** */
 
 g_user.firstName;
 g_user.lastName;
 g_user.userID;
 g_user.userName;
 g_user.getFullName();
-g_user.hasRole("role");
+g_user.hasRole('role');
 
-/* GlideAjax */
 
-var ga = new GlideAjax("HelloWorld");
-ga.addParam("sysparm_name", "helloWorld");
-ga.addParam("sysparm_user_name", "Bob");
+/* ******************** GlideAjax ******************** */
+
+/* ********** Single Parameter ********** */
+
+/* Client Script */
+function onLoad() {
+  var ga = new GlideAjax('scriptIncludeName');
+  ga.addParam('sysparm_name', 'functionName');
+  ga.addParam('sysparm_name2', 'value');
+  ga.getXMLAnswer(functionName);
+}
+function functionName(response) {
+  alert(response);
+}
+
+/* Script include */
+var scriptIncludeName = Class.create();
+scriptIncludeName.prototype = Object.extendsObject(global.AbstractAjaxProcessor, {
+  functionName: function () {
+    var variableName = this.getParameter('sysparm_name2');
+    var grUser = new GlideRecord('sys_user');
+    grUser.get('user_name', variableName);
+    var result = {
+      'name': grUser.getDisplayValue('name')
+    };
+    return JSON.stringify(result);
+  },
+  type: 'scriptIncludeName'
+});
+
+
+/* ********** Multi Parameters ********** */
+
+/* Client Script */
+function onLoad() {
+  var ga = new GlideAjax('scriptIncludeName');
+  ga.addParam('sysparm_name', 'functionName');
+  ga.addParam('sysparm_name2', 'value');
+  ga.getXML(functionName);
+}
+function functionName(response) {
+  var answer = response.responseXML.documentElement.getAttribute('answer');
+  if (answer) {
+    var returneddata = answer.evalJSON(true);
+    alert(returneddata.sys_id, returneddata.name);
+  }
+}
+
+/* Script include */
+var scriptIncludeName = Class.create();
+scriptIncludeName.prototype = Object.extendsObject(AbstractAjaxProcessor, {
+  functionName: function () {
+    var variableName = this.getParameter('sysparm_name2');
+    var gr = new GlideRecord('table');
+    if (gr.get(variableName)) {
+      var json = new JSON();
+      var results = {
+        'sys_id': gr.getValue('sys_id'),
+        'name': gr.getValue('name')
+      };
+      return json.encode(results);
+    }
+  },
+  type: 'scriptIncludeName'
+});
+
+
+/* ********** Asynchronous GlideAjax ********** */
+
+/* Client Script */
+var ga = new GlideAjax('HelloWorld');
+ga.addParam('sysparm_name', 'helloWorld');
+ga.addParam('sysparm_user_name', 'Bob');
 ga.getXML(HelloWorldParse);
-
 function HelloWorldParse(response) {
-  var answer = response.responseXML.documentElement.getAttribute("answer");
+  var answer = response.responseXML.documentElement.getAttribute('answer');
   alert(answer);
 }
 
-var now_GR = new GlideRecord("incident");
+/* Script include */
+var HelloWorld = Class.create();
+HelloWorld.prototype = Object.extendsObject(AbstractAjaxProcessor, {
+  helloWorld: function () { return 'Hello ' + this.getParameter('sysparm_user_name') + '!'; },
+  _privateFunction: function () { // this function is not client callable     
+  }
+});
 
-/* GlideRecord */
 
-var gr = new GlideRecord("table");
-gr.addQuery("fieldName", "value");
+/* ***** Synchronous GlideAjax ***** */
+
+/* Client Script */
+var ga = new GlideAjax('HelloWorld');
+ga.addParam('sysparm_name', 'helloWorld');
+ga.addParam('sysparm_user_name', 'Bob');
+ga.getXMLWait();
+alert(ga.getAnswer());
+
+/* Script include */
+var HelloWorld = Class.create();
+HelloWorld.prototype = Object.extendsObject(AbstractAjaxProcessor, {
+  helloWorld: function () { return 'The Server Says Hello ' + this.getParameter('sysparm_user_name') + '!'; }
+});
+
+
+/* ******************** GlideRecord ******************** */
+
+/* ***** GlideRecord Query ***** */
+
+var gr = new GlideRecord('table');
+gr.addQuery('fieldName', 'value');
 gr.query();
 
 while (gr.next()) {
-  return false;
+  gs.log('State is ' + gr.state);
 }
 
-/* Logs */
+/* ***** GlideRecord Query GET ***** */
 
-gs.log(
-  "logDescription: " +
-    JSON.stringify({
-      name: value,
-      name: value,
-      name: value,
-      name: value,
-    }),
-  "logSource"
-);
+var gr = new GlideRecord('table');
+if (gr.get('active', true)) {
+  gs.log('State is ' + gr.state);
+}
 
-gs.log("Label: " + "value", "logSource");
+/* ***** GlideRecord GetRefRecord ***** */
+
+var caller = current.caller_id.getRefRecord();
+caller.email = 'test@test.com';
+caller.update();
+
+/* ***** GlideRecord Insert ***** */
+
+var gr = new GlideRecord('table');
+gr.initialize();
+gr.short_description = 'Network problem';
+gr.category = 'software';
+gr.caller_id.setDisplayValue('Joe Employee');
+gr.insert();
+
+/* ***** GlideRecord Update ***** */
+
+var gr = new GlideRecord('table');
+gr.addQuery('active', true);
+gr.query();
+while (gr.next()) {
+  gr.active = false;
+  gr.update();
+}
+
+/* ***** GlideRecord Delete ***** */
+var gr = new GlideRecord('table');
+gr.addQuery('active', false);
+gr.query();
+gr.deleteMultiple(); //Deletes all records in the record set
+
+while (gr.next()) {
+  gr.deleteRecord(); //Delete each record in the query result set
+}
+
+/* ***** GlideRecord AddEncodedQuery ***** */
+
+var gr = new GlideRecord('table');
+var strQuery = 'active=true^category=software^ORcategory=hardware';
+gr.addEncodedQuery(strQuery);
+gr.query();
+
+
+/* ******************** Logs ******************** */
+
+gs.log('Label: ' + 'value', 'logSource');
+
+gs.log('logDescription: ' + JSON.stringify({
+  name: value,
+  name: value,
+  name: value,
+  name: value,
+  name: value,
+}), 'logSource');
